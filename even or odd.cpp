de13b62ea9ae3525423f5a24e51abaa762b16a56
Western Morris <270106785@yoobeestudent.ac.nz>
2021-03-04 12:42:41 +1300

    Add project files.

---
+++ even or odd.cpp
@@ -0,0 +1,31 @@
+// even or odd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+	int value;
+
+	std::cout << "whats your number ";
+	std::cin >> value;
+
+	if (value % 2 == 0) 
+
+	std::cout << value << " is even.";
+
+	else
+
+	std::cout << value << " is odd.";
+
+	
+
+
+
+
+}
+
